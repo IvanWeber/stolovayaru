829f238ce3cf4d691433948451cd25a9742caa75
IvanWeber <ivanbdn@yandex.ru>
2021-03-20 12:45:44 +0300

    created preliminary header, styling needed

---
+++ src/assets/js/mobile-search.js
@@ -0,0 +1,15 @@
+console.log('file3');
+
+const mobileSearchForm = document.querySelector('.mobile-search-form')
+const mobileSearchButton = document. querySelector('.header__mobile-search-icon-button')
+
+const mobileSearchFormClickHandler = (event) => {
+
+  if (mobileSearchForm.classList.contains('hidden-completely')) {
+    mobileSearchForm.classList.remove('hidden-completely')
+  } else {
+    mobileSearchForm.classList.add('hidden-completely')
+  }
+}
+
+mobileSearchButton.addEventListener('click', mobileSearchFormClickHandler)
