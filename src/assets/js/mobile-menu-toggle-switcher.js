2ef02f033b4c129e57778f2a36003ed14530b444
IvanWeber <ivanbdn@yandex.ru>
2021-03-26 12:31:05 +0300

    fix menu toggle switcher

--- src/assets/js/mobile-menu-toggle-switcher.js
+++ src/assets/js/mobile-menu-toggle-switcher.js
@@ -1,20 +1,8 @@
 console.log('mobile-menu-toggle-switcher.js enable');
 
-const label = document.querySelector('.header-dropdown-menu')
+const label = document.querySelector('.general-menu__menu-checkbox-label')
 const toggleOpen = document.querySelector('.mobile-menu-toggle--open')
 const toggleClosed = document.querySelector('.mobile-menu-toggle--closed')
-
-toggleOpen.addEventListener('click', () => {
-    toggleOpen.classList.add('hidden-completely')
-    toggleClosed.classList.remove('hidden-completely')
-  }
-)
-
-toggleClosed.addEventListener('click', () => {
-    toggleClosed.classList.add('hidden-completely')
-    toggleOpen.classList.remove('hidden-completely')
-  }
-)
 
 label.addEventListener('click', () => {
   if (toggleClosed.classList.contains('hidden-completely')) {
